c8f0a9cb6e62bd0e25ea219890c74e55f1aef324
fluencerplus <57620606+fluencerplus@users.noreply.github.com>
2019-11-13 19:07:07 +0300

    Update rankdogrulama.js

--- komutlar/rankdogrulama.js
+++ komutlar/rankdogrulama.js
@@ -44,12 +44,14 @@ exports.run = function(client,message,args) {
                 ]
               };
               message.channel.send({ embed });
-              var ranks = ['643363694012792832','643363648269582377','643363555445571584','643363073884684289','643363035372847115','643362860885606410'];
-              for(var i = 0;i<6;i++) {
-                if(message.member.roles.has(ranks[i])) {
-                  await message.member.removeRole(ranks[i]);
-                }
-              }
+              (async function() {
+                  var ranks = ['643363694012792832','643363648269582377','643363555445571584','643363073884684289','643363035372847115','643362860885606410'];
+                  for(var i = 0;i<6;i++) {
+                    if(message.member.roles.has(ranks[i])) {
+                      await message.member.removeRole(ranks[i]);
+                    }
+                  }
+              })();
               switch(true)
               {
                 case score < 1200:
